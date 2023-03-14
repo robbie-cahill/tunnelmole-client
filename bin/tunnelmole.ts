9f60f888b2622417601adbb7ef1dfb07d0f1bf13
Robbie Cahill <robbie-cahill@users.noreply.github.com>
2023-03-14 20:44:27 +1100

    Replace help to refer to new domains

--- bin/tunnelmole.ts
+++ bin/tunnelmole.ts
@@ -19,16 +19,16 @@ async function run()
 
 Get a random public URL: "tmole <port>"
 For example you would run "tmole 80" (without the quotes) if your local server is running on port 80.
-Your server will then be accessible under a random URL like https://f38fg.tmole.sh which will be shown in the output.
+Your server will then be accessible under a random URL like https://f38fg.tunnelmole.com which will be shown in the output.
 This method is free and is a good way to get started.
 
-Get a public URL that does not change: "tmole <port> as <subdomain>.tmole.sh"
-For example you would run "tmole 80 as myapi.tmole.sh" (without the quotes) if your server runs on port 80 and you want to make it available with the domain myapi.tmole.sh
-This method requires a subscription which comes with an API key. Get one at https://dashboard.tmole.sh from $5.99 per month and support the development of this app.
+Get a public URL that does not change: "tmole <port> as <subdomain>.tunnelmole.com"
+For example you would run "tmole 80 as myapi.tunnelmole.com" (without the quotes) if your server runs on port 80 and you want to make it available with the domain myapi.tunnelmole.com
+This method requires a subscription which comes with an API key. Get one at https://dashboard.tunnelmole.com from $5.99 per month and support the development of this app.
 
-Tmole.sh URLs are accessible from any unrestricted internet connection in the world. You don't need special firewall rules or network config, all traffic is routed through this client app from our servers to your local server.
+tunnelmole.com URLs are accessible from any unrestricted internet connection in the world. You don't need special firewall rules or network config, all traffic is routed through this client app from our servers to your local server.
 
-More detailed instructions, cookbooks and more are available at https://tmole.sh/docs
+More detailed instructions, cookbooks and more are available at https://tunnelmole.com/docs
 `
         )
         .version(VERSION)
