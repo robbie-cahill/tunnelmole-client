0e2a022a464eda5600554e05f01396a08baee5ec
Robbie Cahill <robbie-cahill@users.noreply.github.com>
2023-03-18 20:12:16 +1100

    Bunp version

--- bin/tunnelmole.ts
+++ bin/tunnelmole.ts
@@ -7,7 +7,7 @@ sourceMapSupport.install();
 import program from 'commander';
 import dispatchCommand from '../src/cli/dispatch-command.js';
 
-const VERSION = '2.0.6';
+const VERSION = '2.0.7';
 
 async function run()
 {
