9da5bde2cd6013056b1b7a377cc2b40841646af9
Robbie Cahill <robbie-cahill@proton.me>
2023-03-26 21:56:36 +1100

    Post install script

---
+++ bin/postinstall.ts
@@ -0,0 +1,13 @@
+console.info(`
+━━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╭╮
+┃╭╮╭╮┃╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱┃┃
+╰╯┃┃┣┫╭┳━╮╭━╮╭━━┫┃╭╮╭┳━━┫┃╭━━╮
+╱╱┃┃┃┃┃┃╭╮┫╭╮┫┃━┫┃┃╰╯┃╭╮┃┃┃┃━┫
+╱╱┃┃┃╰╯┃┃┃┃┃┃┃┃━┫╰┫┃┃┃╰╯┃╰┫┃━┫
+╱╱╰╯╰━━┻╯╰┻╯╰┻━━┻━┻┻┻┻━━┻━┻━━╯
+Congrats! Tunnelmole is now installed 😃
+Now what?
+- Get a random public URL for a local server: "tmole <port>" e.g. "tmole 80" if your server is running on port 80
+- Get a customized public URL for a local server: "tmole 80 as mysite.tunnelmole.com"
+- Read the docs for more detailed instructions https://tunnelmole.com/docs
+`);
