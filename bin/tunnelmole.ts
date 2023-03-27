db4086e0280362bafebfae52c4fe1b2d6ac98e29
Robbie Cahill <robbie-cahill@proton.me>
2023-03-27 22:33:21 +1100

    Bump version

--- bin/tunnelmole.ts
+++ bin/tunnelmole.ts
@@ -7,7 +7,7 @@ sourceMapSupport.install();
 import program from 'commander';
 import dispatchCommand from '../src/cli/dispatch-command.js';
 
-const VERSION = '2.0.32';
+const VERSION = '2.0.40';
 
 async function run()
 {
