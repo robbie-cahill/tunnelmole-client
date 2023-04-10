0fc6763a1523f2c6eb4fd7631b15003a9846cbde
Robbie Cahill <robbie-cahill@proton.me>
2023-04-10 21:16:11 +1000

    Post install script

--- cjs/bin/postinstall.js
+++ cjs/bin/postinstall.js
@@ -1,3 +1,32 @@
+#!/usr/bin/env node
+// eslint-disable-next-line
+const axios = require("axios");
+
+const installTelemetry = async () => {
+  const telemetryEndpoint = `https://service.tunnelmole.com/tunnelmole-log-telemetry`;
+
+  if (process.env.TUNNELMOLE_TELEMETRY === "0") {
+    return;
+  }
+
+  // Need to find out what percentage of users who have installed Tunnelmole are using incompatible older Node versions
+  // Using axios for commonjs support and maximum backwards compatiblity with old node versions
+  axios
+    .post(telemetryEndpoint, {
+    type: "post-install",
+    data: {
+        nodeVersion: process.version ? process.version : "Unknown",
+        platform: process.platform ? process.platform : "Unknown"
+    }
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function () {
+      // Ignore the error
+    });
+};
+
+installTelemetry();
+
 console.log(`
 ━━━━╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╭╮
 ┃╭╮╭╮┃╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱┃┃
