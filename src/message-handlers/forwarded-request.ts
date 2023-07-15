c01efc801c7e0897f1d4503551be0db78dacb99d
Thomas Ghysels <info@thomasg.be>
2023-07-15 15:38:51 +0200

    Resolve potential gzip encoding issue

--- src/message-handlers/forwarded-request.ts
+++ src/message-handlers/forwarded-request.ts
@@ -10,6 +10,10 @@ export default async function forwardedRequest(forwardedRequestMessage: Forwarde
     const port = options.port;
     const { requestId, url, headers } = forwardedRequestMessage;
 
+    // Prevent issue with gzip encoding
+    delete headers['Accept-Encoding']
+    delete headers['accept-encoding']
+
     // @todo: Once GET is working, add support for all HTTP methods
     const requestOptions : http.RequestOptions = {
         hostname: '127.0.0.1',
