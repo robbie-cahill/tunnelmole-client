58e0e781087e0c7c787375ffd0d949695e3dca96
Robbie Cahill <robbie-cahill@proton.me>
2023-09-11 19:45:19 +1000

    Update message formatting

--- src/message-handlers/domain-already-reserved.ts
+++ src/message-handlers/domain-already-reserved.ts
@@ -2,7 +2,6 @@ import DomainAlreadyReserved from "../messages/domain-already-reserved";
 
 export default function domainAlreadyReserved(message: DomainAlreadyReserved) {
     console.info(
-        `The domain ${message.subdomain}.tunnelmole.net is already reserved by another user. 
-        Please choose a different subdomain. Falling back to a random subdomain`
+        `The domain ${message.subdomain}.tunnelmole.net is already reserved by another user. Please choose a different subdomain. Falling back to a random subdomain`
     );
 }
