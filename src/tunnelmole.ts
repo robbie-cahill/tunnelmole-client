17dcc8728e4aae2a24e9928140ad5a19ca05171e
Robbie Cahill <robbie-cahill@proton.me>
2025-02-02 13:05:58 +1100

    Print a warning to the console when tunnelmole is run with nothing listening on the port it is forwarding to

--- src/tunnelmole.ts
+++ src/tunnelmole.ts
@@ -5,6 +5,7 @@ import { eventHandler, URL_ASSIGNED } from './events/event-handler.js';
 import { setUpAutoReconnect } from './websocket/setup-auto-reconnect.js';
 import { connect } from './websocket/connect.js';
 import { setIsCli } from './websocket/connection-info-service.js';
+import detectPort from 'detect-port';
 
 export default async function tunnelmole(
     options: Options,
@@ -14,13 +15,26 @@ export default async function tunnelmole(
     await initialiseClientId();
     setIsCli(isCli);
 
+    const { port } = options;
+
     // Set port to 3000 if port is not specified
-    if (options.port === undefined) {
+    if (port === undefined) {
         options.port = 3000;
     }
 
     if (options.setApiKey) {
         return "We are simply setting the API key here. No need to set up a tunnel";
+    }
+
+    const availablePort = await detectPort(options.port);
+    if (availablePort == options.port) {
+        console.warn(
+`
+Warning: You currently don't have anything running on port ${port}, which means you might get the standard 503 gateway timeout error when you make a request, since your service is not reachable. 
+
+Please start your service on port ${port} so that requests can reach your service.
+`);
+
     }
 
     const websocket = await connect(options);
